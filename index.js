486ce9b7f3930b0bf08ece7737e078176a562fdb
wenirudeuhas-ui <wenirudeuhas@gmail.com>
2026-06-13 15:10:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
@@ -18,7 +18,7 @@ app.use('/', async (req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star ‼️
 
